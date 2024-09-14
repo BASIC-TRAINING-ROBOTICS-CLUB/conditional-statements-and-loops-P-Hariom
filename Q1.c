a52b87b1ad2abcfcdb1eaa2c28a80801bee27ebd
P-Hariom <hariompatel7137@gmail.com>
2024-09-14 16:20:11 +0000

    first commit

---
+++ Q1.c
@@ -0,0 +1,20 @@
+
+/*
+Q.program to check odd and even numbers
+Ans.
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter an integer: ");
+    scanf("%d",&num);
+
+    if (num % 2 == 0) {
+        printf("%d is even.\n", num);
+    } else {
+        printf("%d is odd.\n", num);
+    }
+
+    return 0;
+}*/
+
